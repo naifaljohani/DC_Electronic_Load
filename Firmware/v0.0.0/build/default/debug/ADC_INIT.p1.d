c3cadef65fc4c591dbd2c4f2612d1eb74fecac6f
نايف حمدي الجهني <naifj@ycat.edu.sa>
2023-05-18 12:01:18 +0300

    v0.0.0

---
+++ Firmware/v0.0.0/build/default/debug/ADC_INIT.p1.d
@@ -0,0 +1,4 @@
+ build/default/debug/ADC_INIT.d  \
+ build/default/debug/ADC_INIT.p1:  \
+ ADC_INIT.c  \
+adc.h 
---
+++ Firmware/v0.0.0/build/default/production/LOAD20210.p1.d
@@ -0,0 +1,6 @@
+ build/default/production/LOAD20210.d  \
+ build/default/production/LOAD20210.p1:  \
+ LOAD20210.c  \
+LDC_ROT.h  \
+GLCD.h  \
+ADC.h 
